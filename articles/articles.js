5f50c343867603cfb82367562c8d1504b5d199d2
ishimweeli <ishimweeli0@gmail.com>
2022-05-09 21:16:28 +0200

    nnnnnn

--- articles/articles.js
+++ articles/articles.js
@@ -1,53 +1,25 @@
 
-
-const first_name = document.getElementById('title');
-const last_name = document.getElementById('author');
-const email = document.getElementById('articleBody');
-const password = document.getElementById('image');
-const error = document.getElementsByClassName('error-message')
-
+const token = sessionStorage.getItem("token")
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const articleBody = document.getElementById('articleBody');
+const img = document.getElementById('img');
 
 const handleArticles = async () => {
-    let message = ''
-
-    if (!password.value || password.value == "") {
-        message = "Please, provide your password"
-    }
-    if (!email.value || email.value == "") {
-        message = "Please, provide your email"
-    }
-    if (!last_name.value || last_name.value == "") {
-        message = "Please, provide your last name"
-    }
-    if (!first_name.value || first_name.value == "") {
-        message = "Please, provide your first name"
-    }
     const data = {
-        image: image.value,
+        img: img.value,
         title: title.value,
         author: author.value,
         articleBody: articleBody.value
-
     }
-    if (title.value == "" || author.value == "" || articleBody.value == "") {
-        error[ 0 ].innerHTML = message;
-    } else {
-        try {
-            await fetch('https://fast-garden-04062.herokuapp.com/articles', {
-                method: 'POST', // or 'PUT'
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            })
-        } catch (err) {
-            // console.log(error)
-            // message="Not registered. Please, try again" 
-            error[ 0 ].innerHTML = "Not registered. Please, try again"
-        }
+    await fetch('https://fast-garden-04062.herokuapp.com/articles', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': token
+        },
+        body: JSON.stringify(data),
+    })
+    console.log(data);
 
-    }
-    sessionStorage.setItem('token', login.token)
-    // if (register.token) window.location.href = "/login/login.html"
 }
-handleArticles()
