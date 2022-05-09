80ac5221a7735acce1533f63913c8ebb35fc0f12
ishimweeli <ishimweeli0@gmail.com>
2022-05-09 21:54:37 +0200

    kkk

--- blogs.js
+++ blogs.js
@@ -25,3 +25,18 @@ const displayData = async () => {
     console.log(data)
 }
 displayData()
+
+
+article_short = function (str, length, ending) {
+    if (length == null) {
+        length = 100;
+    }
+    if (ending == null) {
+        ending = '...';
+    }
+    if (str.length > length) {
+        return str.substring(0, length - ending.length) + ending;
+    } else {
+        return str;
+    }
+};
