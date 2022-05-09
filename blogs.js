ee16b061822664e5439168c64f65dedce82cde6d
ishimweeli <ishimweeli0@gmail.com>
2022-05-09 23:04:19 +0200

    mmmm

--- blogs.js
+++ blogs.js
@@ -14,6 +14,40 @@ const displayData = async () => {
           <p>
           ${article.articleBody}</p>
           </p>
+          <div class="likes">
+          <button class="btn btn-like" id="like">
+    <span class="btn-icon btn--icon-default">
+        <span class="fa fa-heart"></span>
+    </span>
+    <span class="btn-icon btn--icon-liked">
+        <span class="fa fa-heart"></span>
+    </span>
+    <span class="btn-content  btn-content--liked">
+        Liked
+    </span>
+    <span class="btn-content btn-content--default">
+        Like
+    </span>
+</button>
+
+<section id="app">
+    <div class="container_comments">
+      <div class="row">
+        <div class="col-6">
+          <div class="comment">
+        <p v-for="items in item" v-text="items"></p>
+          </div><!--End Comment-->
+          </div><!--End col -->
+          </div><!-- End row -->
+      <div class="row">
+        <div class="col-6">
+      <textarea type="text" class="input" placeholder="Write a comment" v-model="newItem" @keyup.enter="addItem()"></textarea>
+          <button v-on:click="addItem()" class='primaryContained float-right' type="submit">Add Comment</button>
+        </div><!-- End col -->
+      </div><!--End Row -->
+    </div><!--End Container -->
+  </section><!-- end App -->
+  </div>
           <div class="btns">
             <button>Read More</button>
             </div>
@@ -24,12 +58,9 @@ const displayData = async () => {
     containerComponents[ 0 ].innerHTML = ui
     console.log(data)
 }
-displayData()
-
-
-article_short = function (str, length, ending) {
+displayData(articles = function (str, length, ending) {
     if (length == null) {
-        length = 100;
+        length = 20;
     }
     if (ending == null) {
         ending = '...';
@@ -39,4 +70,6 @@ article_short = function (str, length, ending) {
     } else {
         return str;
     }
-};
+})
+article()
+
