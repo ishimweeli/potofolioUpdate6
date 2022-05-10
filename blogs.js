6cc56066d817f730f8d561b279886a52fbb7355f
ishimweeli <ishimweeli0@gmail.com>
2022-05-10 12:54:24 -0500

    blogs

--- blogs.js
+++ blogs.js
@@ -16,40 +16,7 @@ const displayData = async () => {
         <div class="name_job">${article.author}</div>
           <p>
         ${article.articleBody}</p>
-          <div class="likes">
-          <button class="btn btn-like" id="like">
-    <span class="btn-icon btn--icon-default">
-        <span class="fa fa-heart"></span>
-    </span>
-    <span class="btn-icon btn--icon-liked">
-        <span class="fa fa-heart"></span>
-    </span>
-    <span class="btn-content  btn-content--liked">
-        Liked
-    </span>
-    <span class="btn-content btn-content--default">
-        Like
-    </span>
-</button>
-
-<section id="app">
-    <div class="container_comments">
-      <div class="row">
-        <div class="col-6">
-          <div class="comment">
-        <p v-for="items in item" v-text="items"></p>
-          </div><!--End Comment-->
-          </div><!--End col -->
-          </div><!-- End row -->
-      <div class="row">
-        <div class="col-6">
-      <textarea type="text" class="input" placeholder="Write a comment" v-model="newItem" @keyup.enter="addItem()"></textarea>
-          <button v-on:click="addItem()" class='primaryContained float-right' type="submit">Add Comment</button>
-        </div><!-- End col -->
-      </div><!--End Row -->
-    </div><!--End Container -->
-  </section><!-- end App -->
-  </div>
+         
           <div class="btns">
             <button>read more</button>
             </div>
